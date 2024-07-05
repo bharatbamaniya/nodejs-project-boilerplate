487ceabff5ef5e78c63add9f779241c0c54fc2b9
Bharat <bharat@mobiuso.com>
2024-07-06 01:12:55 +0530

    Add backend project structure and app starter

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+app.use(express.json({limit: "16kb"}));
+app.use(express.urlencoded({ extended: false, limit: "16kb" }));
+app.use(express.static("public"));
+
+export { app };
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+import { app } from "./app.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({ path: "./src/.env" });
+
+
+const PORT = process.env.PORT || 8080;
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
+}).catch((err) => {
+  console.error("Mongodb connection error ", err);
+});
