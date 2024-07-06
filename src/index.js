783297276e6a57c6933c8d170a377ccfa9d8d9ba
Bharat <bharat@mobiuso.com>
2024-07-06 15:34:50 +0530

    Add healthcheck route for server health check

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 import { app } from "./app.js";
 import connectDB from "./db/index.js";
 
-dotenv.config({ path: "./src/.env" });
+dotenv.config({ path: "./.env" });
 
 
 const PORT = process.env.PORT || 8080;
